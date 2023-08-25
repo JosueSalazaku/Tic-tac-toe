3295ef8bdd5dcf15146dceb37af1bd62f4fd6465
Josue Sebastiao <josuesalazaku@gmail.com>
2023-08-25 23:37:02 +0200

    Tic-tac-toe

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ const startCells = [
 ]
 
 function createBoard() {
-    startCells.forEach((cell, index) => {
+    startCells.forEach((_cell, index) => {
         const cellElement = document.createElement('div')
          cellElement.classList.add('square')
+         cellElement.id = index
+         cellElement.addEventListener('click', addGo)
          gameBoard.append(cellElement)
     })
 }
 
 createBoard()
+
+function addGo(e) {
+    const goDisplay = document.createElement('div')
+    goDisplay.classList.add('circle')
+    e.target.append(goDisplay)
+}
